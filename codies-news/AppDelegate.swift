42db6299d25b3434dc52e5f7b5aef775c400f9e1
Yang <zyterence@gmail.com>
2018-01-04 17:37:22 +0800

    remove main storyboard

--- codies-news/AppDelegate.swift
+++ codies-news/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = self.window else { return false }
+        
+        window.rootViewController = ViewController()
+        window.backgroundColor = UIColor.white
+        window.makeKeyAndVisible()
         return true
     }
 
